9ab4081dccdc643e3f5120916a56c5d44c3bcc94
OpenAI GPT <noreply@example.com>
2023-05-24 14:42:00 +0000

    chore: fixes from GPT

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 import sys
 
+def bad_method(wrong_name):
+  correct_name = wrong_name
+  print('Out: ', correct_name)
+
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
