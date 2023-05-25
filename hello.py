04d1f070e6cdecbdd0c6ebf0230ec7ac57a48a18
Oleg Kasian <oleg.kasian@gmail.com>
2023-05-25 12:21:41 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
 import sys
-
-def _BadMethod(WrongName):
-  WrongName2 = WrongName
-  print 'Out: ', WrongName2
   
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
